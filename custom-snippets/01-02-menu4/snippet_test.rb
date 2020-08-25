6f39b93fdd6734178cb51286c86eb7a37be494dc
CheezItMan <chris@adadevelopersacademy.org>
2020-08-24 17:57:24 -0700

    rename describe for menu4

--- custom-snippets/01-02-menu4/snippet_test.rb
+++ custom-snippets/01-02-menu4/snippet_test.rb
@@ -5,7 +5,7 @@ require_relative "./submission.rb"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "Test Suite" do
-  describe "list of allergens" do
+  describe "find price" do
     it "will find the price of the last item" do
       price = find_price("molten chocolate cake")
       expect(price).must_equal 8.5
