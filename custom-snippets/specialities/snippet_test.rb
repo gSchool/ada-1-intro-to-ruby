f36e23d52208af0ab793a237fba9484595e44d60
CheezItMan <chris@adadevelopersacademy.org>
2020-08-26 09:32:36 -0700

    specialities tests

--- custom-snippets/specialities/snippet_test.rb
+++ custom-snippets/specialities/snippet_test.rb
@@ -4,10 +4,10 @@ require_relative "./submission.rb"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-def solution(names, specialities)
+def solution(characters)
   answer = []
-  names.each_with_index do |name, index|
-    answer << "#{name}'s specialty is to #{specialities[index].upcase}"
+  characters.each_with_index do |character, index|
+    answer << "#{character[:name]}'s specialty is to #{character[:specialty].upcase}"
   end
 
   return answer
@@ -16,27 +16,30 @@ end
 describe "Test Suite" do
   describe "lengths" do
     it "will return an empty array if given two empty arrays" do
-      names = []
-      specialities = []
-      answer = specialities(names, specialities)
+      characters = []
+      answer = specialities(characters)
 
       expect(answer.length).must_equal 0
     end
 
     it "will return an array of 1 length for two 1-element arrays" do
-      names = ["Charles"]
-      specialities = ["TA"]
-      answer = specialities(names, specialities)
+      characters = [
+        { name: "Al", specialty: "Toolman"},
+      ]
+      answer = specialities(characters)
 
       expect(answer.length).must_equal 1
-      names.each_with_index do |name, index|
-        expect(answer[index]).must_equal "#{name}'s specialty is to #{specialities[index].upcase}"
+      characters.each_with_index do |name, index|
+        expect(answer[index]).must_equal "#{characters[:name]}'s specialty is to #{characters[:specialty].upcase}"
       end
     end
 
     it "will return an array of 3 length for two 3-element arrays" do
-      names = ["Charles", "Simon", "Dan"]
-      specialities = ["TA", "Lead Instructor", "Former Lead Instructor"]
+      characters = [
+        { name: "Simon", specialty: "Lead Instructor"},
+        { name: "Dan", specialty: "Former Lead Instructor"},
+        { name: "Bethany", specialty: "Head Honcho"},
+      ]
       answer = specialities(names, specialities)
       solution = solution(names, specialities)
 
